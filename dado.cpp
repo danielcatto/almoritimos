388eadb05d32bb345b9de3717610be6d0108ad16
danielcatto <danielcatto@hotmail.com>
2020-05-28 00:04:30 -0300

    adicionado algoritimo dado.cpp

---
+++ dado.cpp
@@ -0,0 +1,31 @@
+#include <cstdlib> 
+#include <ctime> 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+srand((unsigned)time(0)); 
+int i, contador, lista_numero[20];
+contador = 0;
+int vezes = 0;
+while (contador < 20)
+{
+    i = (rand()%6)+1; 
+    lista_numero[contador] = i;
+    //cout << i << " " << contador+1 << " | ";
+    contador++;
+}
+cout << "\n";
+for (int listar_contador = 0; listar_contador < 20; listar_contador++)
+{   
+    if (lista_numero[listar_contador] % 2 > 0){
+        vezes+=1;
+        cout << lista_numero[listar_contador] << " ";
+    }
+    
+}
+cout << "\n";
+cout << "Foram sorteadas " << vezes << " faces impares\n\n";
+
+}
